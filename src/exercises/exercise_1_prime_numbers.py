58ca1cd14f9162c8064e3198ed1410ca1270605d
ldaniel <contato@leandrodaniel.com>
2019-12-15 18:43:48 -0300

    Add final version for the prime number exercise

--- src/exercises/exercise_1_prime_numbers.py
+++ src/exercises/exercise_1_prime_numbers.py
@@ -1,6 +1,6 @@
 # Exercise 1: Build a Python program that tests if an integer is prime. Use a FOR loop.
-def is_prime(number):
-    if number == 1:
+def is_prime_using_for(number):
+    if number <= 1:
         return False
 
     for i in range(2, number):
@@ -9,11 +9,24 @@ def is_prime(number):
 
     return True
 
+
 # Exercise 2: Build a Python program that tests if an integer is prime. Use a WHILE loop.
+def is_prime_using_while(number):
+    if number == 1:
+        return False
+
+    i = 2
+    while i < number:
+        if (number % i) == 0:
+            return False
+        i += 1
+
+    return True
 
 
 # The main program.
 while True:
-    print("Enter the number to be tested (type <CTRL + C> to exit): ")
+    print("Enter the number to be tested whether is prime or not (type <CTRL + C> to exit): ")
     input_number = int(input())
-    print(str(input_number) + (" is prime." if is_prime(input_number) else " isn't prime"))
+    print(str(input_number) + (" is prime" if is_prime_using_for(input_number) else " isn't prime") + ", using a For loop.")
+    print(str(input_number) + (" is prime" if is_prime_using_while(input_number) else " isn't prime") + ", using a While loop.\n")
