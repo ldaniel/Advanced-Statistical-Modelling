1dbbc8585c42fdbf441e7d66d5ca123167cfc371
ldaniel <contato@leandrodaniel.com>
2019-12-16 15:12:29 -0300

    Update unit tests by writing the basic ones

--- src/exercises/exercise_2_cpf_checker_unittest.py
+++ src/exercises/exercise_2_cpf_checker_unittest.py
@@ -1,9 +1,32 @@
 import unittest
+from src.exercises.exercise_2_cpf_checker import CPFChecker as CPFCheckerClass
 
 
 class Test_CPF_Checker(unittest.TestCase):
-    def test_cpf_1(self):
-        self.assertEqual(True, False)
+    def test_valid_cpf(self):
+        input_cpf = "86282573068"
+        cpf_checker = CPFCheckerClass(input_cpf)
+        self.assertEqual(cpf_checker.is_valid(), True)
+
+    def test_invalid_cpf(self):
+        input_cpf = "86282573067"
+        cpf_checker = CPFCheckerClass(input_cpf)
+        self.assertEqual(cpf_checker.is_valid(), False)
+
+    def test_invalid_cpf_equal_numbers(self):
+        input_cpf = "11111111111"
+        cpf_checker = CPFCheckerClass(input_cpf)
+        self.assertEqual(cpf_checker.is_valid(), False)
+
+    def test_invalid_cpf_wrong_len(self):
+        input_cpf = "1234"
+        cpf_checker = CPFCheckerClass(input_cpf)
+        self.assertEqual(cpf_checker.is_valid(), False)
+
+    def test_invalid_cpf_only_digits(self):
+        input_cpf = "a6282573068"
+        cpf_checker = CPFCheckerClass(input_cpf)
+        self.assertEqual(cpf_checker.is_valid(), False)
 
 
 if __name__ == '__main__':
